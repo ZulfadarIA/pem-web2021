ec8d297b119270055c766706785898c1ccebfc30
ZulfadarIA <zulfadaralkaf@gmail.com>
2021-11-30 16:24:23 +0700

    Latihan minggu 10

---
+++ repo-minggu10/latihanvariabel-js/variabel.js
@@ -0,0 +1,20 @@
+var nama = "Zulfadar";
+var nim = 111202013044;
+var mata_Kuliah = "Pemweb";
+var angka = 280;
+
+
+
+alert("Selamat datang "+ nama );
+
+document.write("Nama : " + nama + "<br>");
+document.write("NIM : " + nim + "<br>");
+document.write("Mata Kuliah : " + mata_Kuliah + "<br>");
+
+//isi ulang variabel
+angka = 12;
+
+
+
+document.write("angka : " + angka + "<br>");
+
